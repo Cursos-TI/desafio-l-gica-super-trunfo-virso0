540d58de089cc163593ba40325f980f7cebc30ad
will <wilsonluizzzz35@gmail.com>
2025-04-07 23:18:20 +0000

    adicionar as cartas

--- logicaSuperTrunfoNivelNovato.c
+++ logicaSuperTrunfoNivelNovato.c
@@ -18,7 +18,56 @@ int main() {
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
+    printf("\nInsira o nome do estado:\n");
+    fgets(nomeestado1, sizeof(nomeestado1), stdin);
 
+    printf("Insira o codigo da carta:\n");
+    fgets(codigo1, sizeof(codigo1), stdin);
+
+    printf("Insira o nome da cidade:\n");
+    fgets(nome1, sizeof(nome1), stdin);
+
+    printf("Insira a População:\n");
+    scanf("%i", &populacao1);
+
+    printf("Insira o Numero de Pontos Turisticos:\n");
+    scanf("%i", &pontoturistico1);
+
+    printf("Insira a área em km:\n");
+    scanf("%f", &area1);
+
+    printf("Insira o PIB:\n");
+    scanf("%f", &pib1);
+    
+    getchar();
+    printf("Vamos regristrar agora a segunda carta\n");
+    
+    printf("\nInsira o nome do estado:\n");
+    fgets(nomeestado2, sizeof(nomeestado2), stdin);
+
+    printf("Insira o codigo da carta:\n");
+    fgets(codigo2, sizeof(codigo2), stdin);
+
+    printf("Insira o nome da cidade:\n");
+    fgets(nome2, sizeof(nome2), stdin);
+
+    printf("Insira a população:\n");
+    scanf("%i", &populacao2);
+
+    printf("Insira o Numero de Pontos Turisticos:\n");
+    scanf("%i", &pontoturistico2);
+
+    printf("Insira a área em km:\n");
+    scanf("%f", &area2);
+
+    printf("Insira o PIB:\n");
+    scanf("%f", &pib2);
+
+    densidadepopulacional1 = (float)populacao1 / area1;
+    densidadepopulacional2 = (float)populacao2 / area2;
+
+    pibpercapita1 = pib1 / populacao1;
+    pibpercapita2 = pib2 / populacao2;
     // Exemplo:
     // printf("Digite o código da cidade: ");
     // scanf("%s", codigo);
